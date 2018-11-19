9770719595b6e77e5000ccecf1afb03e9cfad09e
Talessa <mjunoygomez@gmail.com>
2018-11-19 19:15:45 +0100

    Initial commit

---
+++ app/src/main/java/veteriapp/com/veteriapp/inicio_Activity.java
@@ -0,0 +1,13 @@
+package veteriapp.com.veteriapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class inicio_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+    }
+}
