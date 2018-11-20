59f78d5a948278d7a574bf8d6ddb62ff02772211
Talessa <mjunoygomez@gmail.com>
2018-11-20 16:10:01 +0100

    añadidas pantallas Contacto y puntuar_opciones. Añadidos elementos visuales a estas ultimas. Añadidas las frases al fichero Strings, añadido color blanco al fichero colors.

---
+++ app/src/main/java/veteriapp/com/veteriapp/puntuar_opciones_Activity.java
@@ -0,0 +1,13 @@
+package veteriapp.com.veteriapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class puntuar_opciones_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_puntuar_opciones);
+    }
+}
